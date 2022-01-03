191ae69f20deeef98666ee9f2c76f086180d227e
TINGCYGF <1127163160@qq.com>
2022-01-03 22:59:19 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    // 配置路径别名
+    alias: {
+      "@": "src",
+      "@styles": "src/styles",
+    }
+  }
 })
