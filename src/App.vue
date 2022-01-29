9d14b718a1b635498e80db310a3ee0de09539f4c
TINGCYGF <1127163160@qq.com>
2022-01-29 16:30:25 +0800

    chore: 测试按需引入

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import {ref} from "vue";
+const count = ref(0)
+const handleClick: Function = () => {
+  console.log('执行')
+  axios.get('/api/get').then((res) => {
+    console.log(res);
+  })
+}
+
+const handleto = () => {
+  console.log('执行')
+}
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-
+  <el-button type="error" @click="handleClick">按钮</el-button>
+  <button @click="()=> {
+    count++
+    handleto()
+  }">啊啊{{count}}</button>
 </template>
 
 <style>
