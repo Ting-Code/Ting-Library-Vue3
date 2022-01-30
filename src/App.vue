a098e9d408fb1f68ef11e184936988447eeea11a
TINGCYGF <1127163160@qq.com>
2022-01-30 11:32:33 +0800

    chore: ESLink配置翻车

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <hello-world></hello-world>
   <el-button type="error" @click="handleClick">按钮</el-button>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios'
-import {ref} from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from 'vue'
 
 const count = ref(0)
 const baseUrl = import.meta.env.VITE_BASE_API
 const handleClick = () => {
-  axios.get(baseUrl as string).then((res) => {
-    console.log(res);
-
+  axios.get(baseUrl as string).then(res => {
+    console.log(res)
   })
 }
 
@@ -23,12 +20,5 @@ const handleto = () => {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
