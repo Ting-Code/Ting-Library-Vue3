5f08ec4e22b927e48c30508679bc76302f6d3e09
TINGCYGF <1127163160@qq.com>
2022-02-03 22:33:54 +0800

    fix: svgIcon

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <router-view />
+  <t-icon name="list" clas="啊啊"></t-icon>
+  <SvgIcon name="sss"></SvgIcon>
+  <div>{{ count }}</div>
+  <el-button type="primary">aaaaaa</el-button>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios';
-import { ref } from 'vue';
-const count = ref(0);
-const baseUrl = import.meta.env.VITE_BASE_API;
+import { ref } from 'vue'
+import SvgIcon from './components/basic/SvgIcon/index.vue'
+const count = ref(12)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.icon {
+  font-size: 50px;
+}
+</style>
