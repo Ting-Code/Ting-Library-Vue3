03ee8f9c54232db1686e9bb17f56c6702b6c9e67
TINGCYGF <1127163160@qq.com>
2022-02-03 22:34:00 +0800

    fix: svgIcon

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    SvgIcon: typeof import('./src/components/basic/SvgIcon/index.vue')['default']
   }
 }
 
