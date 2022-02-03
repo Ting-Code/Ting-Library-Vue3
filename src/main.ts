9b987c7fb9a7ad8618f663fd45ecbf8b25958482
TINGCYGF <1127163160@qq.com>
2022-02-03 22:34:16 +0800

    fix: svgIcon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import installIcon from '@/components'
+import 'virtual:svg-icons-register'
 
-
-createApp(App)
-.use(router)
-.mount('#app')
+const app = createApp(App)
+installIcon(app)
+app
+  .use(router)
+  .mount('#app')
