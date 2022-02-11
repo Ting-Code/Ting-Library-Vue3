0e0c0878757df9a5d9e4b4b62b980729f3bbabf9
ting <1127163160@qq.com>
2022-02-11 19:42:46 +0800

    feat: 环境变量

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,75 @@
-import { createRouter, createWebHistory } from "vue-router"
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory
+} from 'vue-router'
+import layout from '@/layout'
+import UserManageRouter from './modules/UserManage'
+import store from '@/store'
 
+/**
+ * 私有路由表
+ */
+export const privateRoutes = [UserManageRouter]
 
-
-const publicRoutes = [
+/**
+ * 公开路由表
+ */
+export const publicRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index.vue')
+  },
+  {
+    path: '/',
+    component: layout,
+    redirect: '/admin',
+    children: [
+      {
+        path: '/admin',
+        name: 'admin',
+        component: () => import('@/views/admin/index.vue'),
+        meta: {
+          title: 'admin',
+          icon: 'el-icon-user'
+        }
+      },
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404.vue')
+      },
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401.vue')
+      }
+    ]
   }
 ]
 
+/**
+ * 初始化路由表
+ */
+export function resetRouter() {
+  if (
+    store.getters.userInfo &&
+    store.getters.userInfo.permission &&
+    store.getters.userInfo.permission.menus
+  ) {
+    const menus = store.getters.userInfo.permission.menus
+    menus.forEach(menu => {
+      router.removeRoute(menu)
+    })
+  }
+}
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: publicRoutes, 
+  history:
+    process.env.NODE_ENV === 'production'
+      ? createWebHistory()
+      : createWebHashHistory(),
+  routes: publicRoutes
 })
-
 
 export default router
