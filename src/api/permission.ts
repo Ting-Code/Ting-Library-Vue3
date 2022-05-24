a7be668ba252e014425b26f5acc9b5baa0cf002e
ting <1127163160@qq.com>
2022-05-24 20:19:50 +0800

    feat: 个人中心数据

---
+++ src/api/permission.ts
@@ -0,0 +1,14 @@
+import http from '@/utils/http/axios';
+
+
+/**
+ * 获取tree菜单列表
+ * @param params
+ */
+export function getMenuList(params?: any) {
+  return http.request({
+    url: '/menu/list',
+    method: 'GET',
+    params,
+  });
+}
