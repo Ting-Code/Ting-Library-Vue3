8ae6bd7fa5044d4f4fc9c8b9f0d3b41dd4271246
ting <1127163160@qq.com>
2022-06-21 20:00:29 +0800

    feat: scss主题架构

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <AppProvider>
     <router-view />
   </AppProvider>
-  <el-button type="primary">66666666</el-button>
+  <el-button type="primary" @click="change">66666666</el-button>
 </template>
 
 <script setup lang="ts">
 import { AppProvider } from "@/components/Application";
-
+const change = () => {
+  document.documentElement.style.setProperty('--ting-color-primary', 'red')
+  document.documentElement.style.setProperty('--ting-color-primary-light-3', 'red')
+  document.documentElement.style.setProperty('--ting-color-primary-dark-2', 'red')
+}
 </script>
 
 <style lang="scss" scoped>
--- src/components/Application/Application.vue
+++ src/components/Application/Application.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 需要AppProvider包裹都放这里 减少app代码 -->
-  <el-config-provider>
+  <el-config-provider namespace="ting">
     <slot></slot>
   </el-config-provider>
 </template>
