72f1844e8e55e4fdd1d1998a81333c5c6d4738bd
ting <1127163160@qq.com>
2022-06-21 15:15:26 +0800

    feat: scss主题架构

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { setupRouter } from './router'
 import { setupStore } from './store'
 import 'virtual:svg-icons-register'
-// import '@s/index.scss'
+import '@s/index.scss'
 async function bootstrap() {
   const app = createApp(App)
   setupStore(app)
