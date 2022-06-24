09c7eb266a9e8674ec0c9ff88dd0e610067456e8
ting <1127163160@qq.com>
2022-06-24 17:39:46 +0800

    feat(page): layout default

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -3,11 +3,17 @@
     <el-aside width="200px">Aside</el-aside>
     <el-container>
       <el-header>Header</el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        <main>
+          <admin-main />
+        </main>
+      </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </el-container>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AdminMain from './admin-main/index.vue'
+</script>
 
 <style scoped lang="scss"></style>
