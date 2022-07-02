855e1b2dfa93648d4cdd78b3528b74e9d8c54304
ting <1127163160@qq.com>
2022-07-02 14:40:19 +0800

    fix(基础配置): 格式化工具调整

--- types/components.d.ts
+++
@@ -1,9 +0,0 @@
-import SvgIcon from '@/components/Common/Icon/SvgIcon.vue'
-
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    SvgIcon: typeof SvgIcon
-  }
-}
-
-export {}
