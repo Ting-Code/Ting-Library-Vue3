650075a6f841f183dc9a83b24ed0cef9cde39084
ting <1127163160@qq.com>
2022-07-16 14:15:03 +0800

    feat(自定义指令): 权限指令v-auth

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { setupRouter } from './router'
-import { setupStore } from './store'
+import { setupRouter } from '@/router'
+import { setupStore } from '@/store'
+import { setupGlobDirectives } from '@/directives'
+
 import 'virtual:svg-icons-register'
 import '@s/index.scss'
 import 'uno.css'
@@ -9,6 +11,7 @@ async function bootstrap() {
   const app = createApp(App)
   setupStore(app)
   setupRouter(app)
+  setupGlobDirectives(app)
   app.mount('#app')
 }
 
